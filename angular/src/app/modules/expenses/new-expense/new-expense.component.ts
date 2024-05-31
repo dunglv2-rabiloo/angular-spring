2d66bc5d19442052cd27720c7fac649663c5fa9a
dunglv <dunglv2@rabiloo.com>
2024-05-31 09:14:33 +0700

    add new expense page

--- angular/src/app/modules/expenses/new-expense/new-expense.component.ts
+++ angular/src/app/modules/expenses/new-expense/new-expense.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { tablerChevronLeft } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-new-expense',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgIconComponent],
   templateUrl: './new-expense.component.html',
-  styleUrl: './new-expense.component.css'
+  styleUrl: './new-expense.component.css',
+  viewProviders: [provideIcons({ tablerChevronLeft })],
 })
-export class NewExpenseComponent {
-
-}
+export class NewExpenseComponent {}
